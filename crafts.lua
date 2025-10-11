655244fe71c723be39df62901217d7b47baec510
maplemedley <198984680+maplemedley@users.noreply.github.com>
2025-10-11 17:01:08 +0200

    Separate color handling to new module

--- crafts.lua
+++ crafts.lua
@@ -1,100 +1,13 @@
 
 local S = deltaglider.translator
 
+local colors = assert(dofile(core.get_modpath("deltaglider") .. "./colors.lua"))
+
 local has_basic_materials = minetest.get_modpath("basic_materials")
-local has_farming         = minetest.get_modpath("farming")
 local has_pipeworks       = minetest.get_modpath("pipeworks")
 local has_ropes           = minetest.get_modpath("ropes")
-local has_wool            = minetest.get_modpath("wool")
-local has_unifieddyes     = minetest.get_modpath("unifieddyes")
-local dye_prefix_pattern_universal = "^.*dyes?:" -- Known dye prefix matches: dyes, mcl_dyes, mcl_dye, fl_dyes.
-local dye_suffix_pattern_farlands  = "_dye$"     -- A suffix appended to dye names in the Farlands game.
-
-local dye_colors = {
-	black      = "111111",
-	blue       = "0000ff",
-	brown      = "592c00",
-	cyan       = "00ffff",
-	dark_green = "005900",
-	dark_grey  = "444444",
-	green      = "00ff00",
-	grey       = "888888",
-	light_blue = "258ec9",
-	lime       = "60ac19",
-	magenta    = "ff00ff",
-	orange     = "ff7f00",
-	pink       = "ff7f9f",
-	purple     = "6821a0",
-	red        = "ff0000",
-	silver     = "818177",
-	violet     = "8000ff",
-	white      = "ffffff",
-	yellow     = "ffff00",
-}
-
-local translated_colors = {
-	black      = S("Black"),
-	blue       = S("Blue"),
-	brown      = S("Brown"),
-	cyan       = S("Cyan"),
-	dark_green = S("Dark Green"),
-	dark_grey  = S("Dark Grey"),
-	green      = S("Green"),
-	grey       = S("Grey"),
-	light_blue = S("Light Blue"),
-	lime       = S("Lime"),
-	magenta    = S("Magenta"),
-	orange     = S("Orange"),
-	pink       = S("Pink"),
-	purple     = S("Purple"),
-	red        = S("Red"),
-	silver     = S("Light Grey"),
-	violet     = S("Violet"),
-	white      = S("White"),
-	yellow     = S("Yellow"),
-}
 
 
-
-local function get_dye_name(name)
-	-- Remove prefix and potential suffix
-	name = string.gsub(name, dye_suffix_pattern_farlands, "")
-	name = string.match(name, dye_prefix_pattern_universal.."(.+)$")
-	return name
-end
-
-
-
-local function get_dye_color(name)
-	local color
-	if has_unifieddyes then
-		color = unifieddyes.get_color_from_dye_name(name)
-	end
-
-	if not color then
-		color = get_dye_name(name)
-		if color then
-			color = dye_colors[color]
-		end
-	end
-	return color
-end
-
-local function get_color_name(name)
-	return translated_colors[get_dye_name(name)]
-end
-
-local function get_color_name_from_color(color)
-	for name, color_hex in pairs(dye_colors) do
-		if color == color_hex then
-			return translated_colors[name]
-		end
-	end
-	return nil
-end
-
-
--- Recipes ---------------------------------------------------------------------
 -- Materials
 local stick              = xcompat.materials.stick
 local fabric             = "group:wool"
@@ -105,11 +18,11 @@ if core.get_modpath("fl_trees") then
 	string = stick
 end
 
-local repair_items      = {fabric, fabric_repair_only}
-local repair_percentage = 100
 
 
 -- Placeholder repairing recipes (Doesn't directly apply repair, see handler)
+local repair_percentage = 100
+local repair_items      = {fabric, fabric_repair_only}
 minetest.register_craft({
 	output = "deltaglider:glider",
 	recipe = {
@@ -146,20 +59,20 @@ local function crafting_callback_handle_placeholder_recipe(crafted_item, _, old_
 	local wear, repaired, dye_name, color, color_name, repaired = 0, false, nil, nil, nil
 	for _,stack in ipairs(old_craft_grid) do
 		local name = stack:get_name()
-		if name and name ~= "" then
-			if name == "deltaglider:glider" then
-				wear       = stack:get_wear()
-				color      = stack:get_meta():get("glider_color")
-				color_name = get_color_name_from_color(color)
-			elseif minetest.get_item_group(name, "dye") ~= 0 then
-				dye_name = name
-			else
-				for _,repair_item in ipairs(repair_items) do
-					if name == repair_item 
-						or minetest.get_item_group(name, string.match(repair_item, "^group:(.*)$")) ~= 0 
-					then 
-						repaired = true
-					end
+		if not name or name == "" then
+			-- This stack is empty, skip all checks
+		elseif name == "deltaglider:glider" then
+			wear       = stack:get_wear()
+			color      = stack:get_meta():get("glider_color")
+			color_name = colors.get_color_name_from_color(color)
+		elseif minetest.get_item_group(name, "dye") ~= 0 then
+			dye_name = name
+		else
+			for _,repair_item in ipairs(repair_items) do
+				if name == repair_item 
+					or minetest.get_item_group(name, string.match(repair_item, "^group:(.*)$")) ~= 0 
+				then 
+					repaired = true
 				end
 			end
 		end
@@ -167,8 +80,8 @@ local function crafting_callback_handle_placeholder_recipe(crafted_item, _, old_
 
 	-- Overwrite color with dye if present
 	if dye_name then
-		color      = get_dye_color(dye_name)
-		color_name = get_color_name(dye_name)
+		color      = colors.get_dye_color(dye_name)
+		color_name = colors.get_color_name(dye_name)
 	end
 
 	-- Repair if any repair item present
